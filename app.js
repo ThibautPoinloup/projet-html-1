29adfbb2fba7f199abdd0cedf139d2a8fc30878e
thibaut-poinloup <poinloupthibaut@gmail.com>
2023-02-09 09:21:27 +0100

    Ajout d'un carousel

--- app.js
+++ app.js
@@ -1,4 +1,56 @@
 
+//------Carousel------//
+
+class Carrousel {
+    constructor(images, htmlId) {
+        this.images = images;
+        this.htmlId = htmlId;
+        this.carrouselImageContainer = document.getElementById(htmlId);
+        this.indexActuel = 0;
+        this.carouselElement = document.createElement('img'); 
+        this.display();
+    }
+    display = () => {
+        this.carouselElement.setAttribute('alt', "Alternative text");
+        this.carouselElement.setAttribute('src', this.images[this.indexActuel]);
+
+        this.carrouselImageContainer.appendChild(this.carouselElement);
+    }
+    suivant = () => {
+        if (!this.images[this.indexActuel + 1]) {
+            this.indexActuel = 0;
+        } else {
+            this.indexActuel += 1;
+        }
+        this.display()
+    }
+    precedent = () => {
+        
+        if (!this.images[this.indexActuel - 1]) {
+            this.indexActuel = this.images.length - 1;
+        } else {
+            this.indexActuel -= 1;
+        }
+        this.display()
+    }
+}
+
+const imagesArray = ["https://images.pexels.com/photos/255379/pexels-photo-255379.jpeg", "https://images.pexels.com/photos/949587/pexels-photo-949587.jpeg", "https://images.pexels.com/photos/1227511/pexels-photo-1227511.jpeg"]
+const carrousel1 = new Carrousel(imagesArray, "carouselContent")
+
+const previousArrow = document.getElementById('arrowLeft');
+const nextArrow = document.getElementById('arrowRight');
+
+
+previousArrow.addEventListener('click', () => carrousel1.precedent())
+nextArrow.addEventListener('click', () => carrousel1.suivant())
+
+setInterval(() => {
+    carrousel1.suivant()
+}, 3000);
+
+//-----Formulaire-------//
+
 import axios from 'axios';
 const formContact = document.getElementById('formContact')
 
@@ -87,3 +139,4 @@ formContact.addEventListener('submit', (event) =>{
       });
     
 });
+
